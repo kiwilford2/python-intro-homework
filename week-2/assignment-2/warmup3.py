c40540602c5a0011f2a130917842c363dec5dc03
Kiara <172807329+kiwilford2@users.noreply.github.com>
2026-07-26 18:19:49 -0400

    Add completed warmup 3 script

---
+++ week-2/assignment-2/warmup3.py
@@ -0,0 +1,5 @@
+# git log --oneline output:
+# 7b3e1a2 Add completed warmup 1 and warmup 2 scripts
+# f4a9012 Initial commit
+
+print("This week I learned how to use terminal commands, manage git branches, and make commits from the CLI.")
