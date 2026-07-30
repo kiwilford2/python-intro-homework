0829ad196de6ed3fcd9d901af04c6596648658f9
Kiara <172807329+kiwilford2@users.noreply.github.com>
2026-07-30 19:29:19 -0400

    Add week-3 folder

--- week-3/eventadmission.py
+++ week-3/eventadmission.py
@@ -8,12 +8,18 @@ base_price = 40.0
 is_military = input("Are you active military or a veteran? (yes/no): ").lower()
 age = int(input("How old are you? "))
 
-if is_military == "yes":
+if is_military == "yes" and age >= 65:
+    price = base_price * 0.85          #15% combined discount ($34)
+    print(f"Thank you for your service! Senior Military Discount applied: ${price:.2f}.")
+
+elif is_military == "yes":
     price = base_price * 0.90
     print(f"Military Discount Applied! Your ticket price is ${price:.2f}.")
+
 elif age >= 65:
     price = base_price * 0.95
     print(f"Senior Discount Applied! Your ticket price is ${price:.2f}.")
+
 else: 
     price = base_price
     print(f"General Admission: Your ticket price is ${price:.2f}.")
